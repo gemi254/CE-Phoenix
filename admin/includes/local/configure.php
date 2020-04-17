e884c44b5f34e2f62812fb4b93df7de367aaa4be
gemi254 <gemi254@192.168.56.1>
2020-04-17 14:28:45 +0300

    Fix

---
+++ admin/includes/local/configure.php
@@ -0,0 +1,34 @@
+<?php
+  define('HTTP_SERVER', 'http://localhost');
+  define('HTTPS_SERVER', 'http://localhost');
+  define('ENABLE_SSL', false);
+  define('HTTP_COOKIE_DOMAIN', '');
+  define('HTTPS_COOKIE_DOMAIN', '');
+  define('HTTP_COOKIE_PATH', '/phoenix.gr/admin');
+  define('HTTPS_COOKIE_PATH', '/phoenix.gr/admin');
+  define('HTTP_CATALOG_SERVER', 'http://localhost');
+  define('HTTPS_CATALOG_SERVER', 'http://localhost');
+  define('ENABLE_SSL_CATALOG', 'false');
+  define('DIR_FS_DOCUMENT_ROOT', 'Z:/phoenix.gr/');
+  define('DIR_WS_ADMIN', '/phoenix.gr/admin/');
+  define('DIR_WS_HTTPS_ADMIN', '/phoenix.gr/admin/');
+  define('DIR_FS_ADMIN', 'Z:/phoenix.gr/admin/');
+  define('DIR_WS_CATALOG', '/phoenix.gr/');
+  define('DIR_WS_HTTPS_CATALOG', '/phoenix.gr/');
+  define('DIR_FS_CATALOG', 'Z:/phoenix.gr/');
+  define('DIR_WS_CATALOG_IMAGES', DIR_WS_CATALOG . 'images/');
+  define('DIR_WS_CATALOG_LANGUAGES', DIR_WS_CATALOG . 'includes/languages/');
+  define('DIR_FS_CATALOG_LANGUAGES', DIR_FS_CATALOG . 'includes/languages/');
+  define('DIR_FS_CATALOG_IMAGES', DIR_FS_CATALOG . 'images/');
+  define('DIR_FS_CATALOG_MODULES', DIR_FS_CATALOG . 'includes/modules/');
+  define('DIR_FS_BACKUP', DIR_FS_ADMIN . 'backups/');
+  define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
+  define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
+  //DB setup
+  define('DB_SERVER', 'localhost');
+  define('DB_SERVER_USERNAME', 'root');
+  define('DB_SERVER_PASSWORD', '');
+  define('DB_DATABASE', 'phoenix_gr');
+  define('USE_PCONNECT', 'false');
+  define('STORE_SESSIONS', 'mysql');
+  define('CFG_TIME_ZONE', 'Europe/Athens');
---
+++ includes/local/configure.php
@@ -0,0 +1,22 @@
+<?php
+  define('HTTP_SERVER', 'http://localhost');
+  define('HTTPS_SERVER', 'http://localhost');
+  define('ENABLE_SSL', false);
+  define('HTTP_COOKIE_DOMAIN', '');
+  define('HTTPS_COOKIE_DOMAIN', '');
+  define('HTTP_COOKIE_PATH', '/phoenix.gr/');
+  define('HTTPS_COOKIE_PATH', '/phoenix.gr/');
+  define('DIR_WS_HTTP_CATALOG', '/phoenix.gr/');
+  define('DIR_WS_HTTPS_CATALOG', '/phoenix.gr/');
+
+  define('DIR_FS_CATALOG', 'Z:/phoenix.gr/');
+  define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
+  define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
+
+  define('DB_SERVER', 'localhost');
+  define('DB_SERVER_USERNAME', 'root');
+  define('DB_SERVER_PASSWORD', '');
+  define('DB_DATABASE', 'phoenix_gr');
+  define('USE_PCONNECT', 'false');
+  define('STORE_SESSIONS', 'mysql');
+  define('CFG_TIME_ZONE', 'Europe/Athens');
